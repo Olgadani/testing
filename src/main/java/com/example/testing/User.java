d605b3d5361fb7ccea876f3cabdd91922586d385
Olgadani <olga.metizzz@yandex.ru>
2023-02-24 13:46:50 +0400

    hw

--- src/main/java/com/example/testing/User.java
+++ src/main/java/com/example/testing/User.java
@@ -1,4 +1,41 @@
 package com.example.testing;
 
+import java.util.Objects;
+
 public class User {
+    private String login;
+    private String email;
+
+
+
+    public User(String login, String email) {
+        this.login = login;
+        this.email = email;
+
+    }
+    public String getLogin() {
+        return login;
+    }
+
+    public String getEmail() {
+        String s = email;
+        if (s.contains("@") && s.contains(".")) {
+            return s;
+        } else {
+            System.out.println("Wrong email");
+        }
+        return s;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User user)) return false;
+        return Objects.equals(login, user.login) && Objects.equals(email, user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(login, email);
+    }
 }
